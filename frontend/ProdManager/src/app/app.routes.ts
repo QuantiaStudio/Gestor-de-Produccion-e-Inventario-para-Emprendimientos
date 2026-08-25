b922763099dee9e0d9c6353c823acf6e0eff6044
cesiaf <cesiaf.gimenez@gmail.com>
2026-08-25 15:46:49 -0300

    generar componentes base compartidos (navbar y footer)

--- frontend/ProdManager/src/app/app.routes.ts
+++ frontend/ProdManager/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './public/home//home.component';
+import { AboutUsComponent } from './public/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about-us',
+        component: AboutUsComponent
+    }
+
+];
