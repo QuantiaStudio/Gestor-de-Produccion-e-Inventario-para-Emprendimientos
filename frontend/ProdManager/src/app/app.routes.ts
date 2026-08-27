f5dff2fcb928443d70f8757d6b8dbd54615be163
cesiaf <cesiaf.gimenez@gmail.com>
2026-08-27 14:23:50 -0300

    fix: mover componentes y arreglar rutas

--- frontend/ProdManager/src/app/app.routes.ts
+++ frontend/ProdManager/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { AboutUsComponent } from './public/about-us/about-us.component';
 import { PublicLayoutComponent } from './layout/public-layout/public-layout.component';
-import { NewProductFormComponent } from './dashboard/admin/productos/new-product-form/new-product-form.component';
+import { NewProductFormComponent } from './dashboard/admin/features/productos/new-product-form/new-product-form.component';
 import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
 
 export const routes: Routes = [
@@ -15,7 +15,7 @@ export const routes: Routes = [
     ],
   },
   {
-    path: '',
+    path: 'dashboard',
     component: DashboardLayoutComponent,
     children: [
       { path: 'new-product', component: NewProductFormComponent },
--- frontend/ProdManager/src/app/layout/dashboard-layout/dashboard-layout.component.ts
+++ frontend/ProdManager/src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../public/footer/footer.component';
 import { NavbarComponent } from '../../public/navbar/navbar.component';
-import { NewProductFormComponent } from '../../dashboard/admin/productos/new-product-form/new-product-form.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, NewProductFormComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
