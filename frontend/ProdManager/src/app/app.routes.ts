2274cc028c174fdefdaa384ed7b76851b3949e6c
cesiaf <cesiaf.gimenez@gmail.com>
2026-08-27 12:08:45 -0300

    feat: configurar routing de prueba para visualizar formulario de nuevo producto

--- frontend/ProdManager/src/app/app.routes.ts
+++ frontend/ProdManager/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { AboutUsComponent } from './public/about-us/about-us.component';
 import { PublicLayoutComponent } from './layout/public-layout/public-layout.component';
+import { NewProductFormComponent } from './dashboard/admin/productos/new-product-form/new-product-form.component';
+import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
 
 export const routes: Routes = [
   {
@@ -9,7 +11,14 @@ export const routes: Routes = [
     component: PublicLayoutComponent,
     children: [
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'about-us', component: AboutUsComponent },
+      { path: 'about-us', component: AboutUsComponent }
+    ],
+  },
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    children: [
+      { path: 'new-product', component: NewProductFormComponent },
     ],
   },
 
