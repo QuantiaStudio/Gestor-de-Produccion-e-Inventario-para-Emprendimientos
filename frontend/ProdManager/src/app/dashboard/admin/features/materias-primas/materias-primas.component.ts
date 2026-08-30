e1dcbca6556e1f5e95bcb6f9c246098a699571ec
Mauro Mendieta <mauromendieta246@gmail.com>
2026-08-29 21:13:02 -0300

    feat: sacar link roto del sidebar y reutilizar el modal de detalle en la vista de gestion de materia prima

--- frontend/ProdManager/src/app/dashboard/admin/features/materias-primas/materias-primas.component.ts
+++ frontend/ProdManager/src/app/dashboard/admin/features/materias-primas/materias-primas.component.ts
@@ -3,16 +3,18 @@ import { MateriaPrima } from '../../../../models/materia-prima/materia-prima.mod
 import { MateriaPrimaService } from '../../../../services/materia-prima.service';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MateriaPrimaDetalleComponent } from '../../../../materia-prima/materia-prima-detalle/materia-prima-detalle.component';
 
 @Component({
   selector: 'app-materias-primas',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MateriaPrimaDetalleComponent],
   templateUrl: './materias-primas.component.html',
   styleUrl: './materias-primas.component.css'
 })
 export class MateriasPrimasComponent {
   materiasPrimas: MateriaPrima[];
+  materiaPrimaSeleccionada: MateriaPrima | null = null;
 
    categoriasDisponibles = ['Materia Textil', 'Insumos', 'Envases', 'Químicos'];
    unidadesDisponibles = ['kg', 'unidades', 'litros', 'metros'];
@@ -30,6 +32,15 @@ export class MateriasPrimasComponent {
   constructor(private materiaPrimaService: MateriaPrimaService) {
     this.materiasPrimas = this.materiaPrimaService.obtenerMateriasPrimas();
   }
+
+  seleccionar(materiaPrima: MateriaPrima) {
+    this.materiaPrimaSeleccionada = materiaPrima;
+  }
+
+  cerrarDetalle() {
+    this.materiaPrimaSeleccionada = null;
+  }
+
   guardarMateriaPrima() {
     const valoresForm = this.formRegistro.value;
 
