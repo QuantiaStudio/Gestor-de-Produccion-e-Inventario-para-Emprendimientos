3d77a63f3801c7c37d032120d553eab13a4e6138
Mauro Mendieta <mauromendieta246@gmail.com>
2026-09-08 18:38:27 -0300

    feat: implementar listado de materias primas y stock dentro de gestion de movimientos (TK-12-02)

--- frontend/ProdManager/src/app/app.routes.ts
+++ frontend/ProdManager/src/app/app.routes.ts
@@ -27,6 +27,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: DashboardComponent, pathMatch: 'full' },
       { path: 'materias-primas', component: MateriasPrimasComponent },
+      { path: 'movimientos-stock', component: MateriaPrimaListadoComponent },
       { path: 'users', component: UsersComponent },
       { path: 'productos-terminados', component: ProductoTerminadoListadoComponent },
     ],
