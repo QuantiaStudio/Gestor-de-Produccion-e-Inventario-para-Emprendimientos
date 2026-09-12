ef4b91179d14c6f947cb89549e274710616fc0b1
cesiaf <cesiaf.gimenez@gmail.com>
2026-09-12 17:12:01 -0300

    feat: Realizar importaciones necesarias e iniciar constructor

--- frontend/ProdManager/src/app/dashboard/user/features/produccion/produccion.component.ts
+++ frontend/ProdManager/src/app/dashboard/user/features/produccion/produccion.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProduccionService } from '../../../../services/produccion.service';
+import { OrdenProduccion } from '../../../../models/orden-produccion/orden-produccion.model';
 
 @Component({
   selector: 'app-produccion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produccion.component.html',
   styleUrl: './produccion.component.css'
 })
-export class ProduccionComponent {
+export class ProduccionComponent implements OnInit {
+  ordenes: OrdenProduccion[] = [];
+  Ordenseleccionada?: OrdenProduccion;
 
+  constructor(private produccionService: ProduccionService) {}
+
+  ngOnInit(): void {
+    this.ordenes = this.produccionService.obtenerOrdenes();
+  }
+
+  seleccionarOrden(orden: OrdenProduccion): void {
+    this.Ordenseleccionada = orden;
+  }
 }
