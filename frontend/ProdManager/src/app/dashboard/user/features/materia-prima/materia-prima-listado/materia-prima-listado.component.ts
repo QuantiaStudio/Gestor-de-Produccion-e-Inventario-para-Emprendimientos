8d20435364dfd35ca7524cb0c61d4cd7d736dfe3
Mauro Mendieta <mauromendieta246@gmail.com>
2026-09-18 19:19:20 -0300

    refactor: separar feature de materia prima por rol (admin y user)

--- frontend/ProdManager/src/app/dashboard/user/features/materia-prima/materia-prima-listado/materia-prima-listado.component.ts
+++ frontend/ProdManager/src/app/dashboard/user/features/materia-prima/materia-prima-listado/materia-prima-listado.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { MateriaPrima } from '../../models/materia-prima/materia-prima.model';
-import { MateriaPrimaService } from '../../services/materia-prima.service';
+import { MateriaPrima } from '../../../../../models/materia-prima/materia-prima.model';
+import { MateriaPrimaService } from '../../../../../services/materia-prima.service';
 import { MateriaPrimaDetalleComponent } from '../materia-prima-detalle/materia-prima-detalle.component';
 import { MateriaPrimaMovimientoFormComponent } from '../materia-prima-movimiento-form/materia-prima-movimiento-form.component';
 
